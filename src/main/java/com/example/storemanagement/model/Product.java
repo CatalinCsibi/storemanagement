12d27ec0feb9c703fbb3995985571e31574e71e2
catalincsibi <csibicatalin@gmail.com>
2023-12-27 20:08:56 +0200

    Initial commit

---
+++ src/main/java/com/example/storemanagement/model/Product.java
@@ -0,0 +1,22 @@
+package com.example.storemanagement.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private double price;
+    private String description;
+
+}
---
+++ src/main/java/com/example/storemanagement/model/User.java
@@ -0,0 +1,61 @@
+package com.example.storemanagement.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.management.relation.Role;
+import java.util.Collection;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor // check if needed
+@Entity
+@Table (name = "_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
